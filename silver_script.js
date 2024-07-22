9e65222535fdfc84199ea954cc4373ce00aa47ff
Technoresult <139353634+Technoresult@users.noreply.github.com>
2024-07-22 09:32:31 +0530

    silver_script.js

--- silver_script.js
+++ silver_script.js
@@ -2,13 +2,21 @@ let silverPrices = [];
 const initialDisplayCount = 20;
 let currentDisplayCount = initialDisplayCount;
 
+function getTodayDateString() {
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = today.toLocaleString('default', { month: 'short' });
+    const year = today.getFullYear().toString().substr(-2);
+    return `${day}${month}${year}`;
+}
+
 async function fetchSilverPrices() {
     showLoadingSpinner();
     try {
-        // Manually specify the file name to fetch
-        const response = await fetch(`https://raw.githubusercontent.com/Technoresult/GoldPriceCalculator/main/S_21Jul24.json`);
+        const dateString = getTodayDateString();
+        const response = await fetch(`https://raw.githubusercontent.com/Technoresult/GoldPriceCalculator/main/Folder/S_${dateString}.json`);
         const data = await response.json();
-        console.log('Fetched data:', data); // Debugging line
+        console.log('Fetched data:', data);
         if (!data.silver_rates) {
             throw new Error('No silver rates found in the fetched data');
         }
